49bc06022b54a494564b6c141dc2b3380e4763d4
Maggicorp <mvjcasey@gmail.com>
2017-04-30 19:12:33 -0400

    Adds create, update and destroy function to advice

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
   resources :authors, only: [:index, :show]
-  resources :advices, only: [:index, :show]
+  resources :advices, only: [:index, :show, :destroy, :update, :create]
   resources :examples, except: [:new, :edit]
   post '/sign-up' => 'users#signup'
   get '/random' => 'advices#random'
