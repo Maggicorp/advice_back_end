f0ef3c68dd686bcc1432a6c79cf26c21fda7d931
Maggicorp <mvjcasey@gmail.com>
2017-04-30 17:18:58 -0400

    Updates advice to include author
    Or else it won't update due to new model requirements

--- db/examples.rb
+++ db/examples.rb
@@ -4,15 +4,7 @@ author_list = [{ user_name: 'Nora' }, { user_name: 'Lulu' }, { user_name: 'Defau
 
 Author.create(author_list)
 
-seed1_advices = [{ advice_idea: 'Take a nap' }, { advice_idea: 'Drink more coffee' }, { advice_idea: 'Drink less coffee' }, { advice_idea: 'Call your mom' }, { advice_idea: 'Call your dad' }, { advice_idea: 'Eat some candy' }]
 
-seed2_advices = [{ advice_idea: 'Go for a walk' }, { advice_idea: 'Do some push ups' }, { advice_idea: 'Drink some water' }, { advice_idea: 'Do not spend more than you earn' }, { advice_idea: 'Text a friend' }, { advice_idea: 'Clean your room' }]
-
-seed3_advices = [{ advice_idea: 'Pet a dog' }, { advice_idea: 'Turn off your phone' }, { advice_idea: 'Wiggle your toes' }, { advice_idea: 'Listen to music you like' }, { advice_idea: 'Read a book' }, { advice_idea: 'Help someone else out' }]
-
-Advice.create(seed1_advices)
-Advice.create(seed2_advices)
-Advice.create(seed3_advices)
 
 # The data can then be loaded with the rake db:examples (or created alongside
 # the db with db:nuke_pave).
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-seed1_advices = [{ advice_idea: 'Take a nap' }, { advice_idea: 'Drink more coffee' }, { advice_idea: 'Drink less coffee' }, { advice_idea: 'Call your mom' }, { advice_idea: 'Call your dad' }, { advice_idea: 'Eat some candy' }]
+seed1_advices = [{ advice_idea: 'Take a nap', author: Author.last }, { advice_idea: 'Drink more coffee', author: Author.last }, { advice_idea: 'Drink less coffee', author: Author.last }, { advice_idea: 'Call your mom', author: Author.last }, { advice_idea: 'Call your dad', author: Author.last }, { advice_idea: 'Eat some candy', author: Author.first }]
 
-seed2_advices = [{ advice_idea: 'Go for a walk' }, { advice_idea: 'Do some push ups' }, { advice_idea: 'Drink some water' }, { advice_idea: 'Do not spend more than you earn' }, { advice_idea: 'Text a friend' }, { advice_idea: 'Clean your room' }]
+seed2_advices = [{ advice_idea: 'Go for a walk', author: Author.first }, { advice_idea: 'Do some push ups', author: Author.first }, { advice_idea: 'Drink some water', author: Author.first }, { advice_idea: 'Do not spend more than you earn', author: Author.first }, { advice_idea: 'Text a friend', author: Author.first }, { advice_idea: 'Clean your room', author: Author.first }]
 
-seed3_advices = [{ advice_idea: 'Pet a dog' }, { advice_idea: 'Turn off your phone' }, { advice_idea: 'Wiggle your toes' }, { advice_idea: 'Listen to music you like' }, { advice_idea: 'Read a book' }, { advice_idea: 'Help someone else out' }]
+seed3_advices = [{ advice_idea: 'Pet a dog', author: Author.second }, { advice_idea: 'Turn off your phone', author: Author.second }, { advice_idea: 'Wiggle your toes', author: Author.second }, { advice_idea: 'Listen to music you like', author: Author.second }, { advice_idea: 'Read a book', author: Author.second }, { advice_idea: 'Help someone else out', author: Author.second }]
 
 Advice.create(seed1_advices)
 Advice.create(seed2_advices)
