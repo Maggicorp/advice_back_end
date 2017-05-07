3e00fd0eadcf88203fd5cdcec640d4335d39fd24
Maggicorp <mvjcasey@gmail.com>
2017-05-06 21:28:33 -0400

    No changes
    all commented out

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,13 @@
 # frozen_string_literal: true
 #
+
+# seed1_advices = [{ idea: 'Take a nap' }, { idea: 'Drink more coffee' }, { idea: 'Drink less coffee' }, { idea: 'Call your mom' }, { idea: 'Call your dad'}, { idea: 'Eat some candy' }]
+# #
+# seed2_advices = [{ advice_idea: 'Go for a walk', author: Author.first }, { advice_idea: 'Do some push ups', author: Author.first }, { advice_idea: 'Drink some water', author: Author.first }, { advice_idea: 'Do not spend more than you earn', author: Author.first }, { advice_idea: 'Text a friend', author: Author.first }, { advice_idea: 'Clean your room', author: Author.first }]
+#
+# seed3_advices = [{ advice_idea: 'Pet a dog', author: Author.second }, { advice_idea: 'Turn off your phone', author: Author.second }, { advice_idea: 'Wiggle your toes', author: Author.second }, { advice_idea: 'Listen to music you like', author: Author.second }, { advice_idea: 'Read a book', author: Author.second }, { advice_idea: 'Help someone else out', author: Author.second }]
+
+# Advice.create(seed1_advices)
+# Advice.create(seed2_advices)
+# Advice.create(seed3_advices)
+#
